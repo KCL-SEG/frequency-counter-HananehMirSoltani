5722e4d745d402ac1ed31bcda1eb810bbe40e884
HananehMirSoltani <Hananeh Sadat Mir Soltani>
2023-10-11 18:37:22 +0100

    Find the frequency of items

--- frequencies.py
+++ frequencies.py
@@ -1,7 +1,19 @@
 """Frequencies function."""
 """ENTER YOUR SOLUTION HERE!"""
 
+from typing import ItemsView
+
+
 def frequencies(items):
     frequencies = {}
-    # Your code goes here
+    for item in items:
+        if item in frequencies:
+            frequencies[item] += 1
+        else:
+            frequencies[item] = 1
     return frequencies
+items=['a','b','c','b','a','c','a','c','b','a','c']
+result = frequencies(items)
+
+print("Frequency of items:", result)
+
